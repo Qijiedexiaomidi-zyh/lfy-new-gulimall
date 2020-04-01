ca70258879967dbb9558b898fae1d96171825714
Qijiedexiaomidi-zyh <zyh15237228271@163.com>
2020-04-01 10:54:51 +0800

    项目初始化完成了

---
+++ gulimall-ware/src/test/java/com/zyh/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyh.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
